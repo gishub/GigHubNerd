631c83e68841d741b95aa469174f8f880001b847
Sean Zheng <Zheng.Sean@Hotmail.com>
2017-06-09 10:09:27 -0600

    Add project files.

---
+++ GigHubNerd/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GigHubNerd.Startup))]
+namespace GigHubNerd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
